02af8fe97f1a6dfb2255887f84c8b15c3e524ce9
horvatic <shorvatic@gmail.com>
2021-03-12 05:18:32 +0000

    Add actor to monster and party status

--- Monster Prime/js/plugins/Monsters.js
+++ Monster Prime/js/plugins/Monsters.js
@@ -13,46 +13,96 @@
 
 // Refs https://kinoar.github.io/rmmv-doc-web/classes/game_actor.html
 
+dbCatchStore = 'monsterdata/catchStore.json';
+dbMonsterStore = 'monsterdata/monsterStore.json';
 fs = require('fs');
-fs.writeFile('pc.msprime', 'INIT', function (err) {
-});
 
+try {
+  if (!fs.existsSync(dbCatchStore)) {
+    fs.writeFileSync(dbCatchStore, '[]');
+  }
+} catch(err) {
+  console.error(err)
+}
+
+//Export to RPG maker for adding monster to party
 function addMonsters() {
-  const mosnters = getMosntersList();
-  const mosnterPick = parseInt($gameVariables.value(0002));
+  const caughtMosnters = getCaughtMosnters();
+  const mosnters = getMosnters();
   const actorPick = parseInt($gameVariables.value(0003));
-  if(mosnterPick == 0 || mosnters.length <= mosnterPick) {
+  var mosnterPick = parseInt($gameVariables.value(0002));
+  if(actorPick == 0 || mosnterPick < 1) {
+    return
+  }
+  if(actorPick < 1 || actorPick > 8) {
     return;
   }
-  if(actorPick == 0 || actorPick < 1 || actorPick > 8) {
+
+  const mIds = $gameVariables.value(0004).split(",")
+  if(mIds.length <= mosnterPick) {
     return;
   }
-  $gameActors.actor(actorPick).setName(mosnters[mosnterPick])
+  const mosnter = caughtMosnters.find(m => m.id == mIds[mosnterPick]);
+  const mosnterMetaData = mosnters.find(m => m.monster == mosnter.name);
+  $gameActors.actor(actorPick).setName(mosnter.name)
+  $gameActors.actor(actorPick).changeClass(mosnterMetaData.classId, false)
+  $gameActors.actor(actorPick).changeLevel(mosnter.level, false)
+  $gameActors.actor(actorPick).changeExp(mosnter.xp, false)
   $gameParty.addActor(actorPick)
   $gamePlayer.refresh()
+
+  mosnter.inParty = true
+  fs.writeFileSync(dbCatchStore, JSON.stringify(caughtMosnters));
 }
 
-function readMonsters() {
-    const mosnters = getMosntersList();
-    var pcList = "";
+//Export to RPG maker for reading all caught monsters
+function readMonstersNotInParty() {
+    const mosnters = getCaughtMosnters();
+    var mNames = "";
+    var mIds = "buffer"
     mosnters.forEach((m, index) => {
-      if (m != 'INIT') {
-        pcList += index + ": " + m + "\n"
+      if(!m.inParty) {
+        mNames += (index + 1) + ": " + m.name + "\n"
+        mIds += "," + m.id
       }
     });
-    $gameVariables.setValue(0001, pcList);
+    $gameVariables.setValue(0001, mNames);
+    $gameVariables.setValue(0004, mIds);
 }
 
-function catchMonster(name){
-    fs.appendFileSync('pc.msprime', "," + name);
+//Export to RPG maker for catching monsters
+function catchMonster(){
+  const m = getCaughtMosnters();
+  var newMonster = {}
+  newMonster.name = $gameTroop.members()[0].enemy().name
+  newMonster.level = 1
+  newMonster.xp = 0
+  newMonster.inParty = false
+  newMonster.id = uuidv4()
+  m.push(newMonster);
+  fs.writeFileSync(dbCatchStore, JSON.stringify(m));
 }
 
-function getMosntersList() {
+function getMosnters() {
   try {
-    const data = fs.readFileSync('pc.msprime', 'utf8');
-    const mosnters = data.split(',');
-    return mosnters;
+    const data = fs.readFileSync(dbMonsterStore, 'utf8');
+    return JSON.parse(data)
   } catch (err) {
     return null;
   }
+}
+
+function getCaughtMosnters() {
+  try {
+    const data = fs.readFileSync(dbCatchStore, 'utf8');
+    return JSON.parse(data)
+  } catch (err) {
+    return null;
+  }
+}
+
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
 }
